cac72fe40846cc70ffffd89fa21ee63ec65c4708
Esteban Flores <ebflores88@gmail.com>
2020-02-29 06:38:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -10,6 +10,13 @@ var array = [];
 function forLoop(array){
   //expect(strangeArray[34]).to.equal(rest)
   for(var i = 0; i < 25; i++){
+    if(i === 1){
+      array.push(`I am 1 strange loop.`);
+    } else{
+      array.push(`I am ${i} strange loops.`);
+    }
+
+
     /*if(array[i] === undefined){
       array[i] = `I am 1 strange loop.`;
     } else if(i === 1 || i === 11){
