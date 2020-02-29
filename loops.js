021b271f2a4fdf208d957b8cb72792815df81c8b
Esteban Flores <ebflores88@gmail.com>
2020-02-29 05:42:23 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -32,13 +32,13 @@ function whileLoop(n){
   return `done`;
 };
 
-//Define a function called doWhileLoop in loops.js. The function should take an 
-//integer as an argument. Use the incrementVariable() function 
-//(you can copy it from this README) in the condition, and console log 
-//"I run once regardless." while incrementVariable() returns a number less than 
-//the parameter received. (Your condition might look something like 
-//incrementVariable() < num.) Remember that it should also console log when 
-//receiving 0 as a parameter because the do-while runs before the condition is 
+//Define a function called doWhileLoop in loops.js. The function should take an
+//integer as an argument. Use the incrementVariable() function
+//(you can copy it from this README) in the condition, and console log
+//"I run once regardless." while incrementVariable() returns a number less than
+//the parameter received. (Your condition might look something like
+//incrementVariable() < num.) Remember that it should also console log when
+//receiving 0 as a parameter because the do-while runs before the condition is
 //checked.
 
 function doWhileLoop(){
