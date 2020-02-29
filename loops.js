6ada8d87d29c91a439384b5a01c67c21cc99141d
Esteban Flores <ebflores88@gmail.com>
2020-02-29 06:46:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -49,6 +49,6 @@ function incrementVariable() {
 function doWhileLoop(num){
   incrementVariable();
   do{
-    console.log("I run once regardless. while incrementVariable() returns a number less than the parameter received.");
+    console.log("I run once regardless.");
   } while (incrementVariable() < num);
 };
